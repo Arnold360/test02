9dc332cefcfc6021354596167d38abd8559849a6
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2023-10-19 07:32:36 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ruta2 } from 
+import { ruta2 } from './ruta2';
 
 const routes: Routes = [{path: 'ruta2',  component: ruta2}];
 
