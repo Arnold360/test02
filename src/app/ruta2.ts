2f88140edac2fceac35b397f1f16dd62c2f536d3
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2023-10-29 15:05:57 -0500

    Update ruta2.ts

--- src/app/ruta2.ts
+++ src/app/ruta2.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ruta2',
   templateUrl: './ruta2.html',
-  styleUrl: './ruta2.css'
+  styleUrls: ['./ruta2.css']
 })
 export class ruta2 {
   title = 'ruta2';
