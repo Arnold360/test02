9b4c0a6f1e08ddcb4fab9471fb14da5cf83299e3
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2023-11-13 09:18:42 -0500

    Update ruta2.ts

--- src/app/ruta2.ts
+++ src/app/ruta2.ts
@@ -13,7 +13,7 @@ export class ruta2 {
   sources: string[] = ["https://4.bp.blogspot.com/-9AMM6-R4LG8/TaB68k4nOdI/AAAAAAAACaU/61VQ0G6McdQ/s1600/TODO-EL-ARTE-MOCHICA-EN-VASIJA-FIGURA-Y-ABUNDANTE-DECORACION-CON-ASA-ESTRIBO.jpg" , "https://www.meisterdrucke.es/kunstwerke/400w/Moche%20-%20Stirrup%20vase%20in%20the%20form%20of%20a%20man%20playing%20a%20drum%20200-800%20%28ce%20-%20%28MeisterDrucke-259516%29.jpg" , "https://www.tierra-inca.com/album/photos/020/02044.jpg"];
   source = "https://4.bp.blogspot.com/-9AMM6-R4LG8/TaB68k4nOdI/AAAAAAAACaU/61VQ0G6McdQ/s1600/TODO-EL-ARTE-MOCHICA-EN-VASIJA-FIGURA-Y-ABUNDANTE-DECORACION-CON-ASA-ESTRIBO.jpg";
   constructor() {
-  this.element = document.getElementById("boton").style.heigth;
+  this.element = document.getElementById("boton").style.width;
   }
   sizeChange() {
     if (this.count <= 2){
