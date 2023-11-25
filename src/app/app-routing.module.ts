6d48c734f8c78a3e5a4cf315164fa89ef4fb9346
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2023-11-25 08:20:48 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { ruta2 } from './ruta2';
 import { ruta3 } from './ruta3';
 
 const routes: Routes = [{path: 'ruta2',  component: ruta2},
-                        {path: 'ruta3',  component: ruta3}];
+                        {path: 'ruta3',  component: ruta3},
+                        {path: '**', componenr: AppComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
