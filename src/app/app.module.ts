01a1944ae62b9121313bd5f013a200d39255dd88
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2023-12-22 21:54:39 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ruta2 } from './ruta2';
+import { ruta3 } from './ruta3';
+import { ruta4 } from './ruta4';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ruta2
+    ruta2,
+    ruta3,
+    ruta4
   ],
   imports: [
     BrowserModule,
