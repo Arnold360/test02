a442e82eb50fb54ed68da0e6a04d7c83e952be13
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2023-12-30 20:30:07 -0500

    Update ruta4.ts

--- src/app/ruta4.ts
+++ src/app/ruta4.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 export class ruta4 {
   conferencias:string[] = ["CONFERENCIA EN LA PLAZA SAN MARTIN 2022","CONFERENCIA EN ANDAHUAYLAS 2022"];
   mitines:string[] = ["MITIN EN ANDAHUAYLAS 2022","MITIN EN CUSCO 2022"];
-  entrevistas:string[] = ["ENTREVISTA WILLAX 2022","ENTREVISTA LA REPUBLICA 2023"];
+  entrevistas:string[] = ["ENTREVISTA WILLAX 2022","ENTREVISTA LA REPUBLICA 2023", "ENTREVISTA EN CUSCO 2023"];
   declaraciones:string[] = this.conferencias.concat(this.mitines).concat(this.entrevistas);
   videoActual:string = "";
   indice:number = 0;
