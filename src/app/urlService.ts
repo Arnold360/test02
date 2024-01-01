519a391a506d13ed45fe83461d522d18db0b3a51
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-01-01 12:54:57 -0500

    Update urlService.ts

--- src/app/urlService.ts
+++ src/app/urlService.ts
@@ -1,1 +1,11 @@
+import { Injectable } from '@angular/core';
 
+@Injectable({
+  providedIn: 'root',
+})
+export class urlService {
+
+  urls:string[] = ["entrevista1", "entrevista2", "entrevista3"];
+  constructor() { }
+
+}
