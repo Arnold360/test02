bf205a68742dba9afa93ec671b4394353c449fa2
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-01-02 09:27:04 -0500

    Update nombreService.ts

--- src/app/nombreService.ts
+++ src/app/nombreService.ts
@@ -7,6 +7,6 @@ import { Injectable } from '@angular/core';
 export class nombreService {
 
   entrevistas:string[] = ["entrevista1", "entrevista2", "entrevista3"];
-  constructor() { }
-
-}
+  url:string[] = [{nombre:entrevista , url:"https://www.youtube.com/embed/m3hay8aw0Pc?si=s2irA4BQisB1vgMS" }]
+  constructor() {
+  }
