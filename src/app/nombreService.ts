9503b15156b7dca0ba1e052c9a321dedd0210253
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-01-03 07:21:05 -0500

    Update nombreService.ts

--- src/app/nombreService.ts
+++ src/app/nombreService.ts
@@ -6,7 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class nombreService {
 
-  entrevistas:string[] = ["entrevista1", "entrevista2", "entrevista3"];
-  url:string[] = [{nombre:"entrevista" , url:"https://www.youtube.com/embed/m3hay8aw0Pc?si=s2irA4BQisB1vgMS" }];
+  nombres:string[] = ["entrevista1", "entrevista2", "entrevista3"];
+  url:string[] = [];
+  entrevistas:string[] = [{nombre:"entrevista" , url:"https://www.youtube.com/embed/m3hay8aw0Pc?si=s2irA4BQisB1vgMS" }];
   constructor() {
   }
