31837920fe96f851a54e8c72b30dd1269a5057ba
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-01-04 08:52:57 -0500

    Update nombreService.ts

--- src/app/nombreService.ts
+++ src/app/nombreService.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { entrevista } from './entrevista';
+import { declaracion } from './declaracion';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +9,7 @@ export class nombreService {
 
   nombres:string[] = ["entrevista1", "entrevista2", "entrevista3"];
   urls:string[] = [];
-  entrevistas:entrevista[] = [{nombre:"entrevista" , url:"https://www.youtube.com/embed/m3hay8aw0Pc?si=s2irA4BQisB1vgMS" }];
+  declaraciones:declaracion[] = [{nombre:"entrevista" , url:"https://www.youtube.com/embed/m3hay8aw0Pc?si=s2irA4BQisB1vgMS" }];
   constructor() {
     for( let i = 0; this.nombres.length > i; i++) {
       this.entrevistas.push({nombre:this.nombres[i], url:this.urls[i]});
