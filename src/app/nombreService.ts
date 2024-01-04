8010caad0a600348f6dd210d2d6bd92d9b2d1663
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-01-04 08:28:05 -0500

    Update nombreService.ts

--- src/app/nombreService.ts
+++ src/app/nombreService.ts
@@ -11,7 +11,7 @@ export class nombreService {
   urls:string[] = [];
   entrevistas:entrevista[] = [{nombre:"entrevista" , url:"https://www.youtube.com/embed/m3hay8aw0Pc?si=s2irA4BQisB1vgMS" }];
   constructor() {
-    for( let i = 0; this.nombre.length > i; i++) {
+    for( let i = 0; this.nombres.length > i; i++) {
       this.entrevistas.push{{nombre:this.nombres[i], url:this.urls[i]}};
     }
   }
