386f4cd377ac0b05bfcc0a7ef8ed0dd8f1cf19c2
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-01-05 18:34:47 -0500

    Update nombreService.ts

--- src/app/nombreService.ts
+++ src/app/nombreService.ts
@@ -7,8 +7,17 @@ import { declaracion } from './declaracion';
 })
 export class nombreService {
 
-  nombres:string[] = ["entrevista1", "entrevista2", "entrevista3"];
-  urls:string[] = [];
+  nombres:string[] = ["ENTREVISTA A ANTAURO HUMALA TASSO - BEST CALE TV 01/2023", 
+                      "El cuestionado plan de ANTAURO HUMALA: BUKELIZAR el país y retirarnos de la corte IDH | #LR 12/2023",
+                      "ANTAURO HUMALA NO SE ARREPIENTE DE NADA Y PROMETE MEDIDAS RADICALES 08/2023",
+                      "ENTREVISTA EN LA MIRA GO TV CUSCO 13/11/2023",
+                      
+                      ];
+  urls:string[] = ["https://www.youtube.com/embed/HfCjXPMj5VA?si=WcMHdlsjn3uzewKo",
+                   "https://www.youtube.com/embed/6TXE6qFPI7Y?si=-ffjiubZfRxniNeq",
+                   "https://www.youtube.com/embed/ta50OnS7UQo?si=cPlY9EBohYopIEqP",
+                   "https://www.youtube.com/embed/zOO7PIKJd5k?si=t5slTL6tQSpWXAiR",
+                  ];
   declaraciones:declaracion[] = [{nombre:"entrevista" , url:"https://www.youtube.com/embed/m3hay8aw0Pc?si=s2irA4BQisB1vgMS" }];
   constructor() {
     for( let i = 0; this.nombres.length > i; i++) {
