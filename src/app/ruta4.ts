56d1a30d01e5fcb51bb458c1ddd664ec1ba96f7d
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-01-08 09:36:51 -0500

    Update ruta4.ts

--- src/app/ruta4.ts
+++ src/app/ruta4.ts
@@ -1,5 +1,5 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { nombreService } from './nombreService';
 @Component({
   selector: 'ruta4',
