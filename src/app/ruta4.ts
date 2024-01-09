83be53c803377564afc91c67f2d7277840c2a715
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-01-09 09:28:11 -0500

    Update ruta4.ts

--- src/app/ruta4.ts
+++ src/app/ruta4.ts
@@ -20,7 +20,7 @@ export class ruta4 {
 
   
   constructor(public nombreservice:nombreService){
-    test = nombreservice.declaraciones[0].nombre;
+    this.test = nombreservice.declaraciones[0].nombre;
     for (let i = 0; this.declaraciones.length > i; i++){
       this.rutas.push("ruta" + i.toString());  
       this.Nombre.push("c");
