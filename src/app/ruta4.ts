cc54fe4c2722f365a2255cf9d543b5598777cadc
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-01-15 06:41:55 -0500

    Update ruta4.ts

--- src/app/ruta4.ts
+++ src/app/ruta4.ts
@@ -15,7 +15,7 @@ export class ruta4 {
   videoActual:string = "";
   indice:number = 0;
   rutas:string[] = []; 
-  element:HTMLCollection = document.getElementsByClassName("elemento");
+  element:HTMLCollection = document.getElementById("elemento").getElementsByClassName("elemento");
   prueba!:number;
   
   
