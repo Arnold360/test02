a1b59c5a9ee2ec19cb41a3bbd76ded91fde36608
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-01-16 07:38:30 -0500

    Update ruta4.ts

--- src/app/ruta4.ts
+++ src/app/ruta4.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { nombreService } from './nombreService';
 @Component({
   selector: 'ruta4',
@@ -7,7 +7,7 @@ import { nombreService } from './nombreService';
   styleUrls: ['./ruta4.css'],
   providers: [nombreService]
 })
-export class ruta4 {
+export class ruta4 implements AfterViewInit {
   conferencias:string[] = ["CONFERENCIA EN LA PLAZA SAN MARTIN 2022","CONFERENCIA EN ANDAHUAYLAS 2022"];
   mitines:string[] = ["MITIN EN ANDAHUAYLAS 2022","MITIN EN CUSCO 2022"];
   entrevistas:string[] = ["ENTREVISTA WILLAX 2022","ENTREVISTA LA REPUBLICA 2023", "ENTREVISTA EN CUSCO 2023"];
@@ -30,7 +30,7 @@ export class ruta4 {
 
   
 
-      ngAfterContentInit() {
+      ngAfterViewInit() {
     
   }
   
