c4629ce6338f1dc9d845d61328f7d7e6a7a0fc5b
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-01-17 07:24:30 -0500

    Update ruta4.ts

--- src/app/ruta4.ts
+++ src/app/ruta4.ts
@@ -30,7 +30,7 @@ export class ruta4 implements AfterViewInit {
   
 
       ngAfterViewInit() {
-    this.element = document.getElementById("elemento")?.getElementsByClassName("elemento") as HTMLCollection;
+    this.element = document.getElementsByClassName("elemento") as HTMLCollection;
     this.prueba = this.element.length;
   }
   
