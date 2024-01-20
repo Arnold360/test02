7afb3a979fc6f1d421500f1e0bd9419539ffa957
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-01-20 01:56:24 -0500

    Update ruta5.ts

--- src/app/ruta5.ts
+++ src/app/ruta5.ts
@@ -1,11 +1,16 @@
 
 import { Component } from '@angular/core';
+import { nombreService } from './nombreService';
 
 @Component({
   selector: 'ruta5',
   templateUrl: './ruta5.html',
   styleUrls: ['./ruta5.css']
+  providers: [ nombreService ];
 })
 export class ruta5 {
-  
+  enlace:string;
+  constructor(servicio:nombreService){
+    this.enlace = servicio.enlace;
+  } 
 }
