d391f2930bbcc115c5c038152eefaa126482a56f
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-07 10:42:53 -0500

    Update ruta3.ts

--- src/app/ruta3.ts
+++ src/app/ruta3.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { SafeUrl, DomSanitizer} from '@angular/platform-browser';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { nombreService } from './nombreService';
 @Component({
   selector: 'ruta3',
   templateUrl: './ruta3.html',
-  styleUrls: ['./ruta3.css']
-})
-export class ruta3 {
+  styleUrls: ['./ruta3.css'],
   
+})
+export class ruta3 implements AfterViewInit {
+
+  videoActual:string = "";
+  indice:number = 0;
+  rutas:string[] = []; 
+  element!:HTMLCollection; 
+  prueba!:number;
+  prueba2!:number;
+  prueba3:string= "https://www.youtube.com/embed/HfCjXPMj5VA?si=WcMHdlsjn3uzewKo";
+  
+  
+  constructor(private sanitizer:DomSanitizer, public nombreservice:nombreService){
+    
+    for (let i = 0; nombreservice.entrevistas.length > i; i++){
+      this.rutas.push("ruta" + i.toString());  
+      }
+    }
+    
+
+   ngAfterViewInit() {
+    this.element = document.getElementsByClassName("elemento") as HTMLCollection;
+    this.prueba = this.element.length;
+  }
+
+   cambiarEnlace(i:number){
+     this.nombreservice.enlace = this.sanitizer.bypassSecurityTrustResourceUrl(this.nombreservice.declaraciones[i].url);
+     this.nombreservice.nombre = this.nombreservice.declaraciones[i].nombre;
+     this.prueba2 = i;
+     
+   }
+  
+
 }
