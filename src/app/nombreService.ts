ccaecbd4652ad9780cb389698d6f6a5f1d972677
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-07 21:31:27 -0500

    Update nombreService.ts

--- src/app/nombreService.ts
+++ src/app/nombreService.ts
@@ -9,34 +9,36 @@ import { declaracion } from './declaracion';
   
 export class nombreService {
   nombreEntrevistas:string[] = ["ENTREVISTA A ANTAURO HUMALA TASSO - BEST CALE TV 01/2023", 
-                      "El cuestionado plan de ANTAURO HUMALA: BUKELIZAR el país y retirarnos de la corte IDH | #LR 12/2023",
-                      "ANTAURO HUMALA NO SE ARREPIENTE DE NADA Y PROMETE MEDIDAS RADICALES 08/2023",
-                      "ENTREVISTA EN LA MIRA GO TV CUSCO 13/11/2023",
-                      "DETRAS DE LA VERDAD - PIURA 10/10/2023",];
-  nombreMitines:string[] = [];
-  tipoNombres:string[][] = [this.nombreEntrevistas , this.nombreMitines];
+                                "El cuestionado plan de ANTAURO HUMALA: BUKELIZAR el país y retirarnos de la corte IDH | #LR 12/2023",
+                                "ANTAURO HUMALA NO SE ARREPIENTE DE NADA Y PROMETE MEDIDAS RADICALES 08/2023",
+                                "ENTREVISTA EN LA MIRA GO TV CUSCO 13/11/2023",
+                                "DETRAS DE LA VERDAD - PIURA 10/10/2023",];
+     urlEntrevistas:string[] = ["https://www.youtube.com/embed/HfCjXPMj5VA?si=WcMHdlsjn3uzewKo",
+                                "https://www.youtube.com/embed/6TXE6qFPI7Y?si=-ffjiubZfRxniNeq",
+                                "https://www.youtube.com/embed/ta50OnS7UQo?si=cPlY9EBohYopIEqP",
+                                "https://www.youtube.com/embed/zOO7PIKJd5k?si=t5slTL6tQSpWXAiR",
+                                "https://www.youtube.com/embed/QEffxGNTHRQ?si=ss40OZepvpDanJjK"];
   
-  urlEntrevistas:string[] = ["https://www.youtube.com/embed/HfCjXPMj5VA?si=WcMHdlsjn3uzewKo",
-                   "https://www.youtube.com/embed/6TXE6qFPI7Y?si=-ffjiubZfRxniNeq",
-                   "https://www.youtube.com/embed/ta50OnS7UQo?si=cPlY9EBohYopIEqP",
-                   "https://www.youtube.com/embed/zOO7PIKJd5k?si=t5slTL6tQSpWXAiR",
-                   "https://www.youtube.com/embed/QEffxGNTHRQ?si=ss40OZepvpDanJjK"
-                  ];
-  urlMitines:string[] = [];
-  tipoUrls:string[][] = [this.urlEntrevistas, this.urlMitines];
+      nombreMitines:string[] = ["ANTAURO HUMALA MITIN DESDE URUBAMBA - CUSCO 09/2022"];
+         urlMitines:string[] = ["https://www.youtube.com/embed/NAvU6m960CM?si=2WfGkid-FxjRyMuq",];
   
-  entrevistas:declaracion[] = [];
-  mitines:declaracion[] = [];
+      tipoNombres:string[][] = [this.nombreEntrevistas , this.nombreMitines];
+         tipoUrls:string[][] = [this.urlEntrevistas, this.urlMitines];
+    
+  
+      entrevistas:declaracion[] = [];
+          mitines:declaracion[] = [];
   declaraciones:declaracion[][] = [this.entrevistas, this.mitines];
   
   enlace!:SafeUrl;
-  nombre!:string;
+   nombre!:string;
   
-  fechaEntrevistas:string[] = [];
+   fechaEntrevistas:string[] = [];
   tituloEntrevistas:string[] = [];
-  fechaMitines:string[] = [];
-  tituloMitines:string[] = [];
-  fechas:string[][] = [this.fechaEntrevistas, this.fechaMitines];
+  
+       fechaMitines:string[] = [];
+      tituloMitines:string[] = [];
+   fechas:string[][] = [this.fechaEntrevistas, this.fechaMitines];
   titulos:string[][] = [this.tituloEntrevistas, this.tituloMitines];
   
   regex:RegExp = RegExp('\d{1,2}\/\d{1,4}');
