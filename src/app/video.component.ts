63d1e940338da452ee776937722c7d8626c93c41
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-18 10:49:25 -0500

    Update video.component.ts

--- src/app/video.component.ts
+++ src/app/video.component.ts
@@ -1,1 +1,20 @@
+import {component, OnInit} from '@angular/core';
 
+
+@Component({
+  tamplate:'<youtube-player> videoId="XqZsoesa55w"</youtube-player>',
+  selector: 'app-video'
+})
+
+  export class VideoComponent implements Oninit {
+
+  tag!:any;
+  
+
+  ngOnInit(){
+
+    this.tag = document.createElement('script');
+    this.tag.src = "https://www.youtube.com/iframe_api";
+    document.body.appendChild(this.tag);
+    }
+  }
