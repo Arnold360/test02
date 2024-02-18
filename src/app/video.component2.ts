f2d9a5d2e6116138499cddd28d94b9a867fb0f70
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-18 15:53:55 -0500

    Update video.component2.ts

--- src/app/video.component2.ts
+++ src/app/video.component2.ts
@@ -1,1 +1,20 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  template:'<youtube-player videoId="XqZsoesa55w"></youtube-player>',
+  selector: 'app-video2'
+})
+
+  export class VideoComponent2 implements OnInit {
+
+  tag!:any;
+  
+
+  ngOnInit(){
+
+    this.tag = document.createElement('script');
+    this.tag.src = "https://www.youtube.com/player_api";
+    document.body.appendChild(this.tag);
+    }
+  }
