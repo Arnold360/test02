4a5366207fc2f71c361708be11f4590cd4ea80b0
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-18 09:51:09 -0500

    Update ruta6.ts

--- src/app/ruta6.ts
+++ src/app/ruta6.ts
@@ -20,7 +20,7 @@ export class ruta6 {
 constructor(){
   
   this.firstScriptTag = document.getElementsByTagName('script')[0];
-  this.firstScriptTag.parentNode.insertBefore(this.tag, this.firstScriptTag);
+  this.firstScriptTag.parentNode.insertBefore(this.firstScriptTag, document.getElement.ById("marka"));
 
   this.videoId = 'M7lc1UVf-VE';
   this.startSeconds = 36;
