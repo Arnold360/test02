d8141fe67a1f3a1e17bb079d882d69ca122cfbf8
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-18 16:53:00 -0500

    Update video.component2.ts

--- src/app/video.component2.ts
+++ src/app/video.component2.ts
@@ -8,13 +8,62 @@ import {Component, OnInit} from '@angular/core';
 
   export class VideoComponent2 implements OnInit {
 
-  tag!:any;
+    tag!:any;
+  firstScriptTag!:HTMLElement;
   
+  videoId!:String;
+  startSeconds!:Number;
+  endSeconds!:Number;
 
-  ngOnInit(){
+  player!:any;
+  playerConfig!:any;
+  // Load the IFrame Player API code asynchronously.
+constructor(){
+  
+  this.tag = document.createElement('script');
+  this.tag.src = "https://www.youtube.com/player_api";
+  this.firstScriptTag = document.getElementsByTagName('script')[0];
+  this.firstScriptTag.parentNode.insertBefore(this.tag, this.firstScriptTag);
 
-    this.tag = document.createElement('script');
-    this.tag.src = "https://www.youtube.com/player_api";
-    document.body.appendChild(this.tag);
+  this.videoId = 'M7lc1UVf-VE';
+  this.startSeconds = 36;
+  this.endSeconds = 45;
+
+// Replace the 'ytplayer' element with an <iframe> and
+// YouTube player after the API code downloads.
+
+  this.playerConfig = {
+    height: '360',
+    width: '640',
+    videoId: this.videoId,
+    playerVars: {
+      autoplay: 1, // Auto-play the video on load
+      controls: 0, // Show pause/play buttons in player
+      showinfo: 0, // Hide the video title
+      modestbranding: 1, // Hide the Youtube Logo
+      fs: 1, // Hide the full screen button
+      cc_load_policy: 0, // Hide closed captions
+      iv_load_policy: 3, // Hide the Video Annotations
+      start: this.startSeconds,
+      end: this.endSeconds,
+      autohide: 0, // Hide video controls when playing
+    },
+    events: {
+      'onStateChange': this.onStateChange
     }
+   };
+  } 
+  
+ onYouTubePlayerAPIReady() {
+  this.player = new window['YT'].Player('ytplayer', this.playerConfig);
+}
+
+ onStateChange(state:any) {
+  if (state.data === window['YT'].PlayerState.ENDED) {
+    this.player.loadVideoById({
+      videoId: this.videoId,
+      startSeconds: this.startSeconds,
+      endSeconds: this.endSeconds
+    });
   }
+}
