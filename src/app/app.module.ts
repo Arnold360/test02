ed6484961ecdb80b63d3d2ad02cb5c097a16f8a4
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-18 10:02:50 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { YouTubePlayerModule } from '@angular/youtube-player;
+import { YouTubePlayerModule } from '@angular/youtube-player';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
