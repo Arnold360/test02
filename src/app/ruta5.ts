9c5349660a1dd56370a234151097509fccfb1a88
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-20 21:17:10 -0500

    Update ruta5.ts

--- src/app/ruta5.ts
+++ src/app/ruta5.ts
@@ -17,7 +17,7 @@ export class ruta5 {
   constructor(public servicio:nombreService){
     this.prueba = servicio.enlace;
     this.iframe = document.getElementById("vid") as HTMLIFrameElement;
-    this.iframe.onload = this.metodo;
+    
 }
   
  metodo() {
