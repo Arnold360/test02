44d6868578c63339686d53e4117c608597e1b07d
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-21 16:39:21 -0500

    Update ruta7.ts

--- src/app/ruta7.ts
+++ src/app/ruta7.ts
@@ -1,1 +1,24 @@
 
+var button = document.getElementById('play')
+var video = document.getElementById('video');
+var startTime = 10;
+var endTime = 20;
+
+button.addEventListener('click', playVideo, !1);
+
+function playVideo(e) {
+
+    function checkTime() {
+        if (video.currentTime >= endTime) {
+           video.pause();
+        } else {
+           /* call checkTime every 1/10th 
+              second until endTime */
+           setTimeout(checkTime, 100);
+        }
+    }
+
+    video.currentTime = startTime;
+    video.play();
+    checkTime();
+}
