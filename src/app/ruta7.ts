cbf569ffb843b8fd5128c2586faf68745a6dc7d3
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-21 19:43:01 -0500

    Update ruta7.ts

--- src/app/ruta7.ts
+++ src/app/ruta7.ts
@@ -17,7 +17,7 @@ export class ruta7 {
   this.button.addEventListener('click', this.playVideo); }
 
   checkTime() {
-        if (currentTime >= this.endTime) {
+        if (video.currentTime >= this.endTime) {
            this.video.pause();
         } else {
            /* call checkTime every 1/10th 
