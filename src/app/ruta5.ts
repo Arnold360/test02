5aaf6bd5e1bf0b60235ede9efb20ca4d6610d79e
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-22 11:43:54 -0500

    Update ruta5.ts

--- src/app/ruta5.ts
+++ src/app/ruta5.ts
@@ -13,21 +13,40 @@ export class ruta5 {
   iframe!:any;
   video!:any;
   prueba:SafeUrl;
+  button:HTMLElement = document.getElementById('play');
+  endTime:number = 20;
   
   constructor(public servicio:nombreService){
     this.prueba = servicio.enlace;
     this.iframe = document.getElementById("vid") as HTMLIFrameElement;
     this.video = this.iframe.contentWindow.document.body.getElementsByClassName('video-stream html5-main-video')[0];
+    this.button.addEventListener('click', this.playVideo); 
     
-    
+   }
+  
+ 
+
+  checkTime() {
+        if (this.video.currentTime >= this.endTime) {
+           this.video.pause();
+        } else {
+           /* call checkTime every 1/10th 
+              second until endTime */
+           setTimeout(this.checkTime, 100);
+        }
+    }
+
+ playVideo() {
+
    
-    
+    this.checkTime();
+
+    this.video.currentTime = this.startTime;
+    this.video.play();
+    this.checkTime();
 }
   
- metodo() {
  
-   
-  }
    
 
 }
