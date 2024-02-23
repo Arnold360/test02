4b659c1b10f97bb7213025672225541674825247
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-22 21:53:41 -0500

    Update ruta7.ts

--- src/app/ruta7.ts
+++ src/app/ruta7.ts
@@ -7,33 +7,5 @@ import {Component} from "@angular/core";
 
   
 export class ruta7 {
-  button = document.getElementById('play');
-  video =  <HTMLVideoElement> document.getElementById('video');
-  startTime:number = 10;
-  endTime:number = 20;
-  
-  constructor(){ 
-    this.button.addEventListener('click', this.playVideo); 
 
-  }
-
-  checkTime() {
-        if (this.video.currentTime >= this.endTime) {
-           this.video.pause();
-        } else {
-           /* call checkTime every 1/10th 
-              second until endTime */
-           setTimeout(this.checkTime, 100);
-        }
-    }
-
- playVideo() {
-
-   
-    this.checkTime();
-
-    this.video.currentTime = this.startTime;
-    this.video.play();
-    this.checkTime();
-}
 }
