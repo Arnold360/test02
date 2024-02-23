4fdd6e10ffa732de618ef1e46381a2dc706f83c1
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-22 21:07:18 -0500

    Update ruta7.ts

--- src/app/ruta7.ts
+++ src/app/ruta7.ts
@@ -7,8 +7,8 @@ import {Component} from "@angular/core";
 
   
 export class ruta7 {
-  button:HTMLElement = document.getElementById('play');
-  video:HTMLVideoElement = document.getElementById('video') as HTMLVideoElement;
+  button = document.getElementById('play');
+  video = document.getElementById('video');
   startTime:number = 10;
   endTime:number = 20;
   
