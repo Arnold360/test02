482e507c280286a4c92558ec97e6b4c005ac8b81
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-02-29 09:30:01 -0500

    Update ruta7.ts

--- src/app/ruta7.ts
+++ src/app/ruta7.ts
@@ -46,7 +46,7 @@ export class ruta7 implements AfterViewInit, OnInit {
   // Loop
   onStateChange(event:any) {
     if (event.data === 0) {
-      this.player.loadVideoById("VoQ7f2zrF5k", 1800, "large");
+      
       this.player.unMute();
       this.player.playVideo();  
      }
