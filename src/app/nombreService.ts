75631d26be04d56cc2144ac793c94a10c1fbd8b9
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-03-06 20:41:47 -0500

    Update nombreService.ts

--- src/app/nombreService.ts
+++ src/app/nombreService.ts
@@ -57,7 +57,7 @@ export class nombreService {
       for( let i = 0; this.tipoNombres[e].length > i; i++) {
          this.declaraciones[e].push({nombre:this.tipoNombres[e][i], url:this.tipoUrls[e][i], urlYt:this.tipoUrlsYts[e][i]});
          this.fechas[e].push(/\d{1,2}\/(\d{1,2}\/)?\d{1,4}/.exec(this.tipoNombres[e][i])![0]);
-         this.titulos[e].push(/([a-zA-Z-#|:áéíóú/:()]+\s)+(?=\d{1,2}\/(\d{1,2}\/)?\d{1,4})?/.exec(this.tipoNombres[e][i])![0]);
+         this.titulos[e].push(/([a-zA-Z-#|:áéíóú/:)(]+\s)+(?=\d{1,2}\/(\d{1,2}\/)?\d{1,4})?/.exec(this.tipoNombres[e][i])![0]);
         
      
 
