a568ab5da60e634d4779be127bd18d222be98efc
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-03-07 20:18:56 -0500

    Update ruta4.ts

--- src/app/ruta4.ts
+++ src/app/ruta4.ts
@@ -36,7 +36,7 @@ export class ruta4 implements AfterViewInit {
    cambiarEnlace(i:number){
      this.nombreservice.enlace = this.sanitizer.bypassSecurityTrustResourceUrl(this.nombreservice.entrevistas[i].url);
      this.nombreservice.enlaceYt = this.nombreservice.entrevistas[i].urlYt;
-     this.nombreservice.nombre = this.nombreservice.entrevistas[i].nombre;
+     this.nombreservice.titulo = this.nombreservice.tituloEntrevistas[i];
      
      
    }
