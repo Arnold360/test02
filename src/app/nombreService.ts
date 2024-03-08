de7f48a5ba527316bfb1661077b5b79e65f04d4f
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-03-07 21:58:33 -0500

    Update nombreService.ts

--- src/app/nombreService.ts
+++ src/app/nombreService.ts
@@ -10,11 +10,11 @@ import { declaracion } from './declaracion';
 export class nombreService {
    /*nombre fecha y tiempo de highlights en segundos*/
     nombreEntrevistas:string[] = ["ENTREVISTA A ANTAURO HUMALA TASSO - BEST CALE TV 01/2023 t1800 t2400 t3000 t6000 "  , 
-                                  "El cuestionado plan de ANTAURO HUMALA: BUKELIZAR el país y retirarnos de la corte IDH | #LR 12/2023",
-                                  "ANTAURO HUMALA NO SE ARREPIENTE DE NADA Y PROMETE MEDIDAS RADICALES 08/2023",
-                                  "ENTREVISTA EN LA MIRA GO TV CUSCO 13/11/2023",
-                                  "DETRAS DE LA VERDAD - PIURA 10/10/2023",
-                                  "Entrevistas / Invitado: Myr.EP (r) Antauro Humala Tasso | La Noticia Perú 18/07/23"];
+                                  "El cuestionado plan de ANTAURO HUMALA: BUKELIZAR el país y retirarnos de la corte IDH | #LR 12/2023 t1800 t2400 t3000 t6000",
+                                  "ANTAURO HUMALA NO SE ARREPIENTE DE NADA Y PROMETE MEDIDAS RADICALES 08/2023 t1800 t2400 t3000 t6000",
+                                  "ENTREVISTA EN LA MIRA GO TV CUSCO 13/11/2023 t1800 t2400 t3000 t6000",
+                                  "DETRAS DE LA VERDAD - PIURA 10/10/2023 t1800 t2400 t3000 t6000",
+                                  "Entrevistas / Invitado: Myr.EP (r) Antauro Humala Tasso | La Noticia Perú 18/07/23 t1800 t2400 t3000 t6000"];
   
        urlEntrevistas:string[] = ["https://www.youtube.com/embed/HfCjXPMj5VA?si=WcMHdlsjn3uzewKo&amp;rel=0&amp;autoplay=1&amp;color=white&amp;start=40",
                                   "https://www.youtube.com/embed/6TXE6qFPI7Y?si=-ffjiubZfRxniNeq&start=40",
@@ -26,8 +26,8 @@ export class nombreService {
     urlEntrevistasYts:string[] = ["HfCjXPMj5VA", "6TXE6qFPI7Y", "ta50OnS7UQo", "zOO7PIKJd5k", "QEffxGNTHRQ", "xoLQeGL8Enw"];
   
   
-      nombreMitines:string[] = ["ANTAURO HUMALA MITIN DESDE URUBAMBA - CUSCO 09/2022",
-                                'Antauro Humala mitin desde Juliaca "todos juntos forjaremos la segunda Republica" 09/2022' ];
+      nombreMitines:string[] = ["ANTAURO HUMALA MITIN DESDE URUBAMBA - CUSCO 09/2022 t1800 t2400 t3000 t6000",
+                                'Antauro Humala mitin desde Juliaca "todos juntos forjaremos la segunda Republica" 09/2022 t1800 t2400 t3000 t6000' ];
          urlMitines:string[] = ["https://www.youtube.com/embed/NAvU6m960CM?si=2WfGkid-FxjRyMuq",
                                 "https://www.youtube.com/embed/tYaX9DInKC8?si=Ny_ApypSzr_MwFXB",];
       urlMitinesYts:string[] = ["NAvU6m960CM", "tYaX9DInKC8"];
