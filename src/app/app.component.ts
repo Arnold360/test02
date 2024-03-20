5392cf63cffbf5a327153ca75f7632dcbcf3164a
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-03-19 20:14:52 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test02';
-  x = window.matchMedia('(max- width: 700px)');
+  x = window.matchMedia('(max-width: 700px)');
+  
   constructor(){
     this.cambiarColor();
     this.x.addEventListener("change", this.cambiarColor);
   }
+  
   cambiarColor(){
     if(this.x.matches){
       document.body.style.backgroundColor = "red";
