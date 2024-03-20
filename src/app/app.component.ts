f9080a13ea76c9b9e61006189264ee1cea056e17
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-03-19 22:28:00 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   
   constructor(){
     this.cambiarColor();
-    this.x.addEventListener("change", () => { this.cambiarColor } );
+    this.x.addEventListener("change", () => { this.cambiarColor() } );
   }
   
   cambiarColor(){
