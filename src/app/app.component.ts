07ae15d441fd7884faa022770096bb6fd732de5f
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-03-21 20:16:07 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component, AfterViewInit } from '@angular/core';
 export class AppComponent implements AfterViewInit {
   title = 'test02';
   x = window.matchMedia("(max-width: 700px)");
+  elemento!:HTMLElement;
   
   constructor(){
     
@@ -18,16 +19,17 @@ export class AppComponent implements AfterViewInit {
   cambiarColor(){
     if (this.x.matches) {
       document.body.style.backgroundColor = "red";
-      (document.getElementsByClassName("gg-list") as HTMLCollectionOf<HTMLElement>)[0].style.display = "none";
+      this.elemento.style.display = "block";
     }
     else {
       document.body.style.backgroundColor = "blue";
-      
+      this.elemento.style.display = "none";
      
     }
   }
 ngAfterViewInit(){
   this.cambiarColor();
+  this.elemento =  (document.getElementsByClassName("gg-list") as HTMLCollectionOf<HTMLElement>)[0];
 }
   
 }
