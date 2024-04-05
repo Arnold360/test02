de6afd3975fc4ddfbd10704ecdb5fb373b5f5225
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-04-04 19:59:04 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,10 +43,10 @@ ngAfterViewInit(){
   this.elemento4 =  document.getElementById("barranav");
   this.elemento5 =  document.getElementById("fondo");
   this.cambiarColor();
-  this.elemento.onmouseover = this.mouseOver;
+  this.elemento4.onmouseover = this.mouseOver;
 }
   mouseOver(){
     this.elemento4.style.display = "flex";
-    this.elemento4.style.fontSize = "250%";
+    this.elemento4.style.fontSize = "100%";
   }
 }
