1cc5054bf2c429d3ca8a9e19ebc3174c0a66ddc5
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-05-19 09:32:02 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ruta4 } from './ruta4';
 import { ruta5 } from './ruta5';
 import { ruta6 } from './ruta6';
 import { ruta7 } from './ruta7';
-import { Data } from './Data';
+
 import { nombreService } from './nombreService';
 import { VideoComponent } from './video.component';
 import { VideoComponent2 } from './video.component2';
@@ -19,7 +19,6 @@ import { YtPlayerComponent } from './yt-player.component';
 @NgModule({
   declarations: [
     AppComponent,
-    Data,
     ruta2,
     ruta3,
     ruta4,
