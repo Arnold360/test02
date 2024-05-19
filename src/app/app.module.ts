cd37689ee94b6cc9d895f35c545cff42e98614e2
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-05-19 12:23:45 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ruta4 } from './ruta4';
 import { ruta5 } from './ruta5';
 import { ruta6 } from './ruta6';
 import { ruta7 } from './ruta7';
-
+import { Data } from './Data';
 import { nombreService } from './nombreService';
 import { VideoComponent } from './video.component';
 import { VideoComponent2 } from './video.component2';
