4a5ae14cf82c3c067d45799999da379b7791fddc
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-05-25 21:17:28 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,6 @@ const routes: Routes = [  { path: 'ruta2',  component: ruta2 },
                           { path: 'ruta3',  component: ruta3,  children: [{path:'ruta5', component:ruta5}] },
                           { path: 'ruta4',  component: ruta4,  children: [{path:'ruta5', component:ruta5}] }, 
                           { path: 'ruta6',  component: ruta6,  children: [{path:'ruta5', component:ruta5}] },
-                          { path: 'ruta7',  component: ruta7 },
                           { path: 'ruta8',  component: ruta8,  children: [{path:'ruta5', component:ruta5}] }  ];
 
 @NgModule({
