070579cebaac01e51d3582debca95f034faf0116
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-05-27 08:15:22 -0500

    Update ruta3.ts

--- src/app/ruta3.ts
+++ src/app/ruta3.ts
@@ -13,9 +13,7 @@ export class ruta3 implements AfterViewInit {
   
   constructor(private sanitizer:DomSanitizer, public nombreservice:nombreService){
     
-    for (let i = 0; nombreservice.mitines.length > i; i++){
-      this.rutas.push("ruta" + i.toString());  
-      }
+      
     }
     
 
