f305e8df0220cda2561c6c0fef3314c0421e9a08
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-06-12 14:51:34 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,7 +58,7 @@ ngAfterViewInit(){
   this.elemento4 = document.getElementById("barranav");
   this.elemento5 = document.getElementById("fondo");
   this.elemento6 = document.getElementById("emvoltura");
-  this.elementoColoreado = document.getElementById("link" + (this.nombreservice.linkActual));
+  this.elementoColoreado = document.getElementById("link" + 4 );
   this.cambiarColor();
   
 }
