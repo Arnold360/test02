1c4723baefcf00635466e0964ad032aada131813
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-06-13 08:19:40 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+deimport { Component, AfterViewInit } from '@angular/core';
 import { nombreService } from './nombreService';
 @Component({
   selector: 'app-root',
@@ -60,7 +60,7 @@ ngAfterViewInit(){
   this.elemento6 = document.getElementById("emvoltura");
   /* random html element as place holder*/
   this.elementoColoreado = document.getElementById("link" + 3 );
-  this.changeLinkColor(document.getElementById("link" + 4));
+  this.changeLinkColor(document.getElementById("link" + 3));
   this.cambiarColor();
   
 }
