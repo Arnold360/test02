0fa69c914eb095282cb337502e32f794dfd3cc98
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-06-25 23:35:25 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,14 @@ import { ruta6 } from './ruta6';
 import { ruta8 } from './ruta8';
 import {AppComponent} from './app.component';
 
-const routes: Routes = [  { path: 'ruta2',  component: ruta2 },
-                          { path: 'ruta3',  component: ruta3,  children: [{path:'ruta5', component:ruta5}] },
-                          { path: 'ruta4',  component: ruta4,  children: [{path:'ruta5/:linkActual', component:ruta5}] }, 
-                          { path: 'ruta6',  component: ruta6,  children: [{path:'ruta5', component:ruta5}] },
-                          { path: 'ruta8',  component: ruta8,  children: [{path:'ruta5', component:ruta5}] }  ];
+const routes: Routes = [  { path: 'app.component/:linkActual', component: AppComponent, 
+                            children: [
+                           { path: 'ruta2',  component: ruta2 },
+                           { path: 'ruta3',  component: ruta3,  children: [{path:'ruta5', component:ruta5}] },
+                           { path: 'ruta4',  component: ruta4,  children: [{path:'ruta5/:linkActual', component:ruta5}] }, 
+                           { path: 'ruta6',  component: ruta6,  children: [{path:'ruta5', component:ruta5}] },
+                           { path: 'ruta8',  component: ruta8,  children: [{path:'ruta5', component:ruta5}] } 
+                                      ] } ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
