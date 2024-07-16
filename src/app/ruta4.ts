27f50da6cbfbc553b69fbb4c2c536590ab5e70c4
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-07-15 23:53:31 -0500

    Update ruta4.ts

--- src/app/ruta4.ts
+++ src/app/ruta4.ts
@@ -43,7 +43,7 @@ export class ruta4 implements AfterViewInit {
      this.elementoVideo = document.getElementById("video");
      this.elementoColoreado = document.getElementById("enlace" + (this.nombreservice.entrevistas.length - 1));
      
-     setTimeout(() => { () => { this.cambiarEnlace(this.nombreservice.entrevistas.length - 1); }, 1000 );
+     setTimeout(() => { this.cambiarEnlace(this.nombreservice.entrevistas.length - 1); }, 1000 );
      }
   
 
