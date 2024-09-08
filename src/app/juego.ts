0025bdf3e7eb61befafc490ba3f57e0003c4c6db
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-09-07 20:30:23 -0500

    Update juego.ts

--- src/app/juego.ts
+++ src/app/juego.ts
@@ -7,70 +7,18 @@ import { Component, ElementRef, HostListener } from '@angular/core';
   styleUrls: ['./juego.css'],
 })
 export class Juego {
-  private canvas: HTMLCanvasElement;
-  private ctx: CanvasRenderingContext2D;
-
-  private ballX = 300;
-  private ballY = 200;
-  private ballSpeedX = 5;
-  private ballSpeedY = 5;
-
-  private paddle1Y = 150;
-  private paddle2Y = 150;
-  paddleWidth: number = 20;
-  paddleColission: boolean = false;
-  test!: Touch;
 
 
-   leftPaddleY = 200;
+
+  leftPaddleY = 200;
   rightPaddleY = 200;
-/* Assuming a two-finger touch
-TouchEvent.touches = [
-  {
-    "identifier": 1,
-    "target": HTMLDivElement,
-    "screenX": 100,
-    "screenY": 200,
-    "clientX": 50,
-    "clientY": 100,
-    "pageX": 50,
-    "pageY": 100,
-    "radiusX": 5,
-    "radiusY": 5,
-    "rotationAngle": 0,
-    "force": 1
-  },
-  {
-    "identifier": 2,
-    "target": HTMLDivElement,
-    "screenX": 300,
-    "screenY": 400,
-    "clientX": 250,
-    "clientY": 300,
-    "pageX": 250,
-    "pageY": 300,
-    "radiusX": 5,
-    "radiusY": 5,
-    "rotationAngle": 0,
-    "force": 1
-  }
-];*/
+  ballX = 390;
+  ballY = 290;
+  ballSpeedX = 2;
+  ballSpeedY = 2;
 
 
-  @HostListener('touchstart',['$event'])
-  tactilPaleta1Abajo(evento: TouchEvent){ this.changePaddle1(50); 
-                                        this.test = evento.touches[0];
-                                        }
-  
-  @HostListener('window:keydown.s', ['$event'])
-  moverPaleta1Abajo(){ this.changePaddle1(50); }
-  @HostListener('window:keydown.w', ['$event'])
-  moverPaleta1Arriba(){ this.changePaddle1(-50); }
 
-  @HostListener('window:keydown.l', ['$event'])
-  moverPaleta2Abajo(){ this.changePaddle2(50); }
-  @HostListener('window:keydown.o', ['$event'])
-  moverPaleta2Arriba(){ this.changePaddle2(-50); }
 
   @HostListener('window:touchstart', ['$event'])
   onTouch(event: TouchEvent) {
@@ -94,76 +42,47 @@ TouchEvent.touches = [
     }
   }
 
+   ngOnInit() {
+    this.startGame();
+  }
 
-
-  
-  constructor(private elementRef: ElementRef) {}
-
-  ngAfterViewInit() {
-    this.canvas = this.elementRef.nativeElement.querySelector('canvas');
-    this.ctx = this.canvas.getContext('2d');
-
+  startGame() {
     setInterval(() => {
-      this.clearCanvas();
-      this.drawBall();
-      this.drawPaddles();
-      this.colission();
       this.updateBallPosition();
-      this.paddleColission = false;
-    }, 16);
+    }, 16); // Approximately 60 frames per second
   }
 
-  private clearCanvas() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  }
-
-  private drawBall() {
-    this.ctx.beginPath();
-    this.ctx.arc(this.ballX, this.ballY, 10, 0, Math.PI * 2);
-    this.ctx.fillStyle = 'white';
-    this.ctx.fill();
-  }
-
-  private drawPaddles() {
-    this.ctx.beginPath();
-    this.ctx.rect(0, this.paddle1Y, this.paddleWidth, 100);
-    this.ctx.fillStyle = 'white';
-    this.ctx.fill();
-
-    this.ctx.beginPath();
-    this.ctx.rect(this.canvas.width - this.paddleWidth, this.paddle2Y, this.paddleWidth, 100);
-    this.ctx.fillStyle = 'white';
-    this.ctx.fill();
-  }
-  
-  changePaddle1(n: number) {
-     this.paddle1Y += n;
-  }
-  changePaddle2(n: number) {
-    this.paddle2Y += n;
-  }
-  colission() {
-    if(this.ballX <= this.paddleWidth && this.ballY <= this.paddle1Y + 50 && this.ballY >= this.paddle1Y - 50) {
-       this.paddleColission = true;
-    }
-    if(this.ballX >= (this.canvas.width - (this.paddleWidth + 10)) && this.ballY <= this.paddle2Y + 50 && this.ballY >= this.paddle2Y - 50) {
-       this.paddleColission = true;
-    }
-    
-  }
-
-  private updateBallPosition() {
+  updateBallPosition() {
     this.ballX += this.ballSpeedX;
     this.ballY += this.ballSpeedY;
-    
-    if (this.ballY <= 0 || this.ballY >= this.canvas.height - 10) {
-      this.ballSpeedY *= -1;
+
+    // Check for collision with top and bottom walls
+    if (this.ballY <= 0 || this.ballY >= window.innerHeight - 20) {
+      this.ballSpeedY = -this.ballSpeedY;
     }
-    
-    if (this.ballX <= 0 || this.ballX >= this.canvas.width - 10 || this.paddleColission) {
-      this.ballSpeedX *= -1;
+
+    // Check for collision with paddles
+    if (this.ballX <= 20 && this.ballY >= this.leftPaddleY && this.ballY <= this.leftPaddleY + 100) {
+      this.ballSpeedX = -this.ballSpeedX;
+    } else if (this.ballX >= window.innerWidth - 40 && this.ballY >= this.rightPaddleY && this.ballY <= this.rightPaddleY + 100) {
+      this.ballSpeedX = -this.ballSpeedX;
+    }
+
+    // Check for out of bounds
+    if (this.ballX <= 0 || this.ballX >= window.innerWidth - 20) {
+      this.resetBall();
     }
   }
+
+  resetBall() {
+    this.ballX = window.innerWidth / 2 - 10;
+    this.ballY = window.innerHeight / 2 - 10;
+    this.ballSpeedX = 2;
+    this.ballSpeedY = 2;
+  }
+
+
+ 
       
 }
 
