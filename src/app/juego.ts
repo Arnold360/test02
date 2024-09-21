2a135323bcc72e9e9d3a1a4754619b41b647f070
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-09-21 17:42:45 -0500

    Update juego.ts

--- src/app/juego.ts
+++ src/app/juego.ts
@@ -20,9 +20,7 @@ export class Juego {
   rightScore = 0;
   canvas = document.getElementById('tennisCourt')! as HTMLCanvasElement;
   ctx = this.canvas.getContext('2d')!;
-  Ball: {canvasWidth:number, canvasHeight:number, radius:number, dx:number,
-         dy:number} = {canvasWidth: this.canvas.width / 2, canvasHeight: this.canvas.height / 2, radius: 20, dx: 2,  dy: 2};
-
+  
 
  @HostListener('window:touchstart', ['$event'])
   onTouch(event: TouchEvent) {
