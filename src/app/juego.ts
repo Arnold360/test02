9bd09703771f81639a00ad6b35dcf4f3d98ff33d
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-09-26 22:29:27 -0500

    Update juego.ts

--- src/app/juego.ts
+++ src/app/juego.ts
@@ -123,7 +123,7 @@ drawSilverBall(ctx: CanvasRenderingContext2D, x: number, y: number, radius: numb
       const brickHeight = 40;
       for (let y = 0; y < height; y += brickHeight) {
         for (let x = 0; x < width; x += brickWidth) {
-          this.drawBrickWithGradient(this.ctx, x, y, brickWidth, brickHeight);
+          this.drawBrickWithGradient(this.ctx, x, y, brickWidth, brickHeight, 10, 14);
          }
        }
      }
