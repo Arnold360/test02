8552edd42768e12db9c5c6d8f5ca1e58c2650b2c
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2024-10-07 14:16:57 -0500

    Update CrearInka.ts

--- src/app/CrearInka.ts
+++ src/app/CrearInka.ts
@@ -17,75 +17,78 @@ export class CrearInka {
   drawInka(imageData: ImageData) {
     const centerX = 400;
     const centerY = 300;
+    const cabezaRadio = 75;
+    const cuerpoAncho = 150;
+    const cuerpoAlto = 250;
 
     // Cabeza
-    for (let y = 100; y < 250; y++) {
-      for (let x = centerX - 75; x < centerX + 75; x++) {
-        if (((x - centerX) ** 2) / (75 ** 2) + ((y - 175) ** 2) / (75 ** 2) < 1) {
+    for (let y = centerY - cabezaRadio; y < centerY + cabezaRadio; y++) {
+      for (let x = centerX - cabezaRadio; x < centerX + cabezaRadio; x++) {
+        if (((x - centerX) ** 2) / (cabezaRadio ** 2) + ((y - centerY) ** 2) / (cabezaRadio ** 2) < 1) {
           this.setPixel(imageData, x, y, 255, 204, 153, 255);
         }
       }
     }
 
     // Cejas
-    for (let x = centerX - 70; x < centerX - 40; x++) {
-      let y = centerY - 220 - Math.sin((x - (centerX - 70)) * Math.PI / 40) * 10;
+    for (let x = centerX - 60; x < centerX - 30; x++) {
+      let y = centerY - 120 - Math.sin((x - (centerX - 60)) * Math.PI / 40) * 10;
       this.setPixel(imageData, x, Math.round(y), 0, 0, 0, 255);
     }
-    for (let x = centerX + 40; x < centerX + 70; x++) {
-      let y = centerY - 220 - Math.sin((x - (centerX + 40)) * Math.PI / 40) * 10;
+    for (let x = centerX + 30; x < centerX + 60; x++) {
+      let y = centerY - 120 - Math.sin((x - (centerX + 30)) * Math.PI / 40) * 10;
       this.setPixel(imageData, x, Math.round(y), 0, 0, 0, 255);
     }
 
     // Ojos
-    for (let y = centerY - 240; y < centerY - 210; y++) {
-      for (let x = centerX - 70; x < centerX - 30; x++) {
-        if (Math.sqrt((x - (centerX - 50)) ** 2 + (y - (centerY - 225)) ** 2) < 20) {
+    for (let y = centerY - 140; y < centerY - 110; y++) {
+      for (let x = centerX - 50; x < centerX - 10; x++) {
+        if (Math.sqrt((x - (centerX - 30)) ** 2 + (y - (centerY - 125)) ** 2) < 20) {
           this.setPixel(imageData, x, y, 255, 255, 255, 255);
         }
-        if (Math.sqrt((x - (centerX - 50)) ** 2 + (y - (centerY - 225)) ** 2) < 15) {
+        if (Math.sqrt((x - (centerX - 30)) ** 2 + (y - (centerY - 125)) ** 2) < 15) {
           this.setPixel(imageData, x, y, 0, 100, 255, 255);
         }
-        if (Math.sqrt((x - (centerX - 50)) ** 2 + (y - (centerY - 225)) ** 2) < 5) {
+        if (Math.sqrt((x - (centerX - 30)) ** 2 + (y - (centerY - 125)) ** 2) < 5) {
           this.setPixel(imageData, x, y, 0, 0, 0, 255);
         }
       }
-      for (let x = centerX + 30; x < centerX + 70; x++) {
-        if (Math.sqrt((x - (centerX + 50)) ** 2 + (y - (centerY - 225)) ** 2) < 20) {
+      for (let x = centerX + 10; x < centerX + 50; x++) {
+        if (Math.sqrt((x - (centerX + 30)) ** 2 + (y - (centerY - 125)) ** 2) < 20) {
           this.setPixel(imageData, x, y, 255, 255, 255, 255);
         }
-        if (Math.sqrt((x - (centerX + 50)) ** 2 + (y - (centerY - 225)) ** 2) < 15) {
+        if (Math.sqrt((x - (centerX + 30)) ** 2 + (y - (centerY - 125)) ** 2) < 15) {
           this.setPixel(imageData, x, y, 0, 100, 255, 255);
         }
-        if (Math.sqrt((x - (centerX + 50)) ** 2 + (y - (centerY - 225)) ** 2) < 5) {
+        if (Math.sqrt((x - (centerX + 30)) ** 2 + (y - (centerY - 125)) ** 2) < 5) {
           this.setPixel(imageData, x, y, 0, 0, 0, 255);
         }
       }
     }
 
     // Nariz
-    for (let y = centerY - 200; y < centerY - 180; y++) {
+    for (let y = centerY - 100; y < centerY - 80; y++) {
       for (let x = centerX - 10; x < centerX + 10; x++) {
         this.setPixel(imageData, x, y, 255, 204, 153, 255);
       }
     }
+
     // Boca
-    for (let y = centerY - 170; y < centerY - 150; y++) {
+    for (let y = centerY - 70; y < centerY - 50; y++) {
       for (let x = centerX - 30; x < centerX + 30; x++) {
         this.setPixel(imageData, x, y, 255, 0, 0, 255);
       }
     }
-
     // Cuello
-    for (let y = centerY - 120; y < centerY - 80; y++) {
-      for (let x = centerX - 20 - (y - (centerY - 100)) / 2; x < centerX + 20 + (y - (centerY - 100)) / 2; x++) {
+    for (let y = centerY - 50; y < centerY - 20; y++) {
+      for (let x = centerX - 20 - (y - (centerY - 35)) / 2; x < centerX + 20 + (y - (centerY - 35)) / 2; x++) {
         this.setPixel(imageData, x, y, 255, 218, 185, 255);
       }
     }
 
     // Torso
-    for (let y = centerY - 80; y < centerY + 120; y++) {
-      for (let x = centerX - 50; x < centerX + 50; x++) {
+    for (let y = centerY - 20; y < centerY + 130; y++) {
+      for (let x = centerX - cuerpoAncho / 2; x < centerX + cuerpoAncho / 2; x++) {
         this.setPixel(imageData, x, y, 0, 0, 0, 255);
       }
     }
@@ -119,7 +122,7 @@ export class CrearInka {
     }
 
     // Piernas
-    for (let y = centerY + 120; y < centerY + 200; y++) {
+    for (let y = centerY + 130; y < centerY + 210; y++) {
       for (let x = centerX - 50; x < centerX - 20; x++) {
         if (((x - (centerX - 35)) ** 2) / (15 ** 2) + ((y - (centerY + 160)) ** 2) / (40 ** 2) < 1) {
           this.setPixel(imageData, x, y, 255, 218, 185, 255);
@@ -131,8 +134,9 @@ export class CrearInka {
         }
       }
     }
+
     // Pies
-    for (let y = centerY + 200; y < centerY + 220; y++) {
+    for (let y = centerY + 210; y < centerY + 230; y++) {
       for (let x = centerX - 70; x < centerX - 40; x++) {
         if (((x - (centerX - 55)) ** 2) / (15 ** 2) + ((y - (centerY + 210)) ** 2) / (10 ** 2) < 1) {
           this.setPixel(imageData, x, y, 255, 218, 185, 255);
@@ -144,5 +148,25 @@ export class CrearInka {
         }
       }
     }
+
+    // Detalles de los pies
+    for (let y = centerY + 220; y < centerY + 230; y++) {
+      for (let x = centerX - 60; x < centerX - 50; x++) {
+        this.setPixel(imageData, x, y, 0, 0, 0, 255);
+      }
+      for (let x = centerX + 50; x < centerX + 60; x++) {
+        this.setPixel(imageData, x, y, 0, 0, 0, 255);
+      }
+    }
+
+    // Sombreado
+    for (let y = centerY - 200; y < centerY + 230; y++) {
+      for (let x = centerX - 100; x < centerX + 100; x++) {
+        if (((x - centerX) ** 2) / (100 ** 2) + ((y - centerY) ** 2) / (150 ** 2) < 1) {
+          this.setPixel(imageData, x, y, 50, 50, 50, 255);
+        }
+      }
+    }
   }
 }
+
