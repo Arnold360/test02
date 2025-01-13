56f71c6d79c27dbff6d96676bf2527b8bad4e015
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2025-01-12 20:05:08 -0500

    Update juego.ts

--- src/app/juego.ts
+++ src/app/juego.ts
@@ -10,8 +10,8 @@ import { CrearInka } from './CrearInka';
 })
 export class Juego implements OnInit, OnDestroy {
  
-  public leftPaddleY = 300;
-  public rightPaddleY = 500;
+  public leftPaddleY = 0;
+  public rightPaddleY = 0;
   ballSpeedX = 2;
   ballSpeedY = 2;
   leftScore = 0;
