24854b7f58813cc482ac83ccd2d35762c7527f77
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2025-01-15 15:24:42 -0500

    Update juego.ts

--- src/app/juego.ts
+++ src/app/juego.ts
@@ -32,6 +32,9 @@ export class Juego implements OnInit, OnDestroy {
   cordenada3!:number;
   innerWidth!:any;
   courtRect2!:any;
+  realisticPaddleY!:any;
+  leftRealPaddleY!:any;
+  rightRealPaddleY!:any;
   
 
   constructor(private websocketService: webSocketService) { }
@@ -68,6 +71,7 @@ export class Juego implements OnInit, OnDestroy {
     const courtRect = (event.target as HTMLElement).getBoundingClientRect();
     const touchX = touch.clientX - courtRect.left;
     const touchY = touch.clientY + (this.courtRect2.top - courtRect.top);
+    this.realisticPaddleY = touch.clientY - courtRect.top;
     this.cordenada = courtRect.top;
     this.cordenada2 = touch.clientY;
     this.cordenada3 = touch.clientY - courtRect.top;
@@ -82,8 +86,10 @@ export class Juego implements OnInit, OnDestroy {
    movePaddle(side: 'left' | 'right', y: number) {
     if (side === 'left') {
       this.leftPaddleY = y - 25; // Center the paddle on the touch point
+      this.leftRealPaddleY = this.realisticPaddleY;
     } else {
       this.rightPaddleY = y - 25;
+      this.rightRealPaddleY = this.realisticPaddleY;
     }
   }
 
