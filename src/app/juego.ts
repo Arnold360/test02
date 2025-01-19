73971f184f0176a07568b362308ad99a03a5c37d
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2025-01-18 20:21:34 -0500

    Update juego.ts

--- src/app/juego.ts
+++ src/app/juego.ts
@@ -77,7 +77,7 @@ export class Juego implements OnInit, OnDestroy {
 
     if (touchX < courtRect.width / 2) {
       this.movePaddle('left', touchY);
-      this.realLeftPaddleY = ((touch.clientY / 2.6)  + (this.courtRect2.top - courtRect.top));
+      this.realLeftPaddleY = (((touch.clientY + this.courtRect2.top - courtRect.top) / 2.6) - 185);
     } else {
       this.movePaddle('right', touchY);
       this.realRightPaddleY = (touch.clientY / 2.6) - 185;
