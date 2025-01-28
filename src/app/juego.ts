4580bd862274907c73a407b6d6dc68cbf946a3a7
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2025-01-28 07:23:26 -0500

    Update juego.ts

--- src/app/juego.ts
+++ src/app/juego.ts
@@ -360,8 +360,12 @@ drawBrickWithGradient(ctx: CanvasRenderingContext2D, x: number, y: number, width
 
   usarPoder(poder:HTMLButtonElement) {
     if(poder.parentElement.id === "poderP1"){
-      if(poder.className === "fuego"){}
-      if(poder.className === "hielo"){}
+      if(poder.className === "fuego"){
+        this.dx = 500;
+      }
+      if(poder.className === "hielo"){
+         this.dx = 100;
+      }
       if(poder.className === "racimo"){}
     }
     if(poder.parentElement.id === "poderP2"){
