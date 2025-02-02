c68b6913fd1b02bbbd76aa6725b27a54aa45f945
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2025-02-01 20:06:41 -0500

    Update juego.ts

--- src/app/juego.ts
+++ src/app/juego.ts
@@ -403,7 +403,7 @@ drawBrickWithGradient(ctx: CanvasRenderingContext2D, x: number, y: number, width
 
     if ( this.controladorDeCantidad ) {
       
-       this.controladorDeCantidad = false;
+       
       
        for(let i = 0 ; i < 5; i++){
          this.update2(canvas, deltaTime, i)
