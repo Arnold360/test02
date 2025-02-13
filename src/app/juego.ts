49da9e1cda8fa2c2aa0822735aaf80aacac1b0a0
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2025-02-13 13:40:10 -0500

    Update juego.ts

--- src/app/juego.ts
+++ src/app/juego.ts
@@ -452,7 +452,7 @@ drawBrickWithGradient(ctx: CanvasRenderingContext2D, x: number, y: number, width
             this.controladorDeCantidad = true;
          }
          else if(poder.className === "campoDeFuerza"){
-            this.drawHalo(canvas.width - 20, this.realRightPaddleY, this.haloOuterRadius, this.haloInnerRadius:number);
+            this.drawHalo(canvas.width - 20, this.realRightPaddleY, this.haloOuterRadius, this.haloInnerRadius:number, this.canvas );
          }
        }
    
