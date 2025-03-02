9a68a9230e65b947a764cc3d1f129e205ea6936e
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2025-03-02 08:52:37 -0500

    Update juego.ts

--- src/app/juego.ts
+++ src/app/juego.ts
@@ -215,9 +215,10 @@ export class Juego implements OnInit, OnDestroy {
         }
     if (this.controladorDeBote == 1) {
 
-          if ( this.x - this.radius < margenLeftDePaleta + 20 - Math.abs(this.realLeftPaddleY - this.y) &&
+          /*if ( this.x - this.radius < margenLeftDePaleta + 20 - Math.abs(this.realLeftPaddleY - this.y) &&
                this.y - this.radius < this.realLeftPaddleY + margenLeftDePaleta  &&  
-               this.y + this.radius > this.realLeftPaddleY - margenLeftDePaleta && this.dx < 0) {
+               this.y + this.radius > this.realLeftPaddleY - margenLeftDePaleta && this.dx < 0) {*/
+            if ( Math.sqrt( (Math.abs(this.realLeftPaddleY - this.y) ** 2) + (Math.abs(20 - this.x) ** 2) ) > this.haloOuterRadius ) {
             
                   this.controladorDeBote = 0;
             
