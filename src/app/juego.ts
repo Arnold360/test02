e4deffa84e086a062356d4c96cb54bd599b33d48
Arnold360 <102887568+Arnold360@users.noreply.github.com>
2025-03-06 21:51:46 -0500

    Update juego.ts

--- src/app/juego.ts
+++ src/app/juego.ts
@@ -223,7 +223,7 @@ export class Juego implements OnInit, OnDestroy {
             
                  this.controladorDeBote = 0;
                  let respaldo = this.dy;
-                 this.dy = this.dy + (((this.dx + this.dy) / this.haloOuterRadius) * (this.realLeftPaddleY - this.y));
+                 this.dy = -this.dy + (((this.dx + this.dy) / this.haloOuterRadius) * (this.realLeftPaddleY - this.y));
                  this.dx = -this.dx - (this.dy - respaldo);
               
                  while ( Math.sqrt( Math.pow(Math.abs(this.realLeftPaddleY - this.y), 2) + Math.pow(Math.abs(20 - this.x), 2) ) 
